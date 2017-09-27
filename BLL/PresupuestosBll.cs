17483437f9d01e4c0e38e1b895220d2476a32ad3
Jose Ariel Paredes Gonzalez <jose_paredes2@ucne.edu.do>
2017-09-27 11:06:21 +0100

    JoseGonzalez

---
+++ BLL/PresupuestosBll.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Entidades;
+using DAL;
+using System.Linq.Expressions;
+
+namespace BLL
+{
+    public class PresupuestosBll
+    {
+        public static bool Guardar(Presupuestos presupuestos)
+        {
+            using (var reposi = new Repositorio<Presupuestos>())
+            {
+                try
+                {
+                    if (Buscar(p => p.PresupuestoId == presupuestos.PresupuestoId) == null)
+                    {
+                        return reposi.Guardar(presupuestos);
+                    }
+                    else
+                    {
+                        return reposi.Modificar(presupuestos);
+                    }
+                }
+                catch (Exception)
+                {
+                    throw;
+                }
+            }
+        }
+
+        public static bool Modificar(Presupuestos presupuestos)
+        {
+            bool modifica = false;
+            using (var reposi = new Repositorio<Presupuestos>())
+            {
+                modifica = reposi.Modificar(presupuestos);
+            }
+
+            return modifica;
+        }
+
+        public static bool Eliminar(Presupuestos presupuestos)
+        {
+            bool eliminado = false;
+            using (var reposi = new Repositorio<Presupuestos>())
+            {
+                eliminado = reposi.Eliminar(presupuestos);
+            }
+            return eliminado;
+        }
+
+        public static Presupuestos Buscar(Expression<Func<Entidades.Presupuestos, bool>> Busqueda)
+        {
+            Presupuestos Result = null;
+            using (var repoitorio = new Repositorio<Presupuestos>())
+            {
+                Result = repoitorio.Buscar(Busqueda);
+            }
+            return Result;
+        }
+
+        public static List<Presupuestos> Listar(Expression<Func<Presupuestos, bool>> busqueda)
+        {
+            List<Presupuestos> Result = null;
+            using (var repoitorio = new Repositorio<Presupuestos>())
+            {
+                try
+                {
+                    Result = repoitorio.Lista(busqueda).ToList();
+                }
+                catch
+                {
+
+                }
+                return Result;
+            }
+        }
+
+        public static List<Presupuestos> ListarTodo()
+        {
+            List<Presupuestos> Result = null;
+            using (var repoitorio = new Repositorio<Presupuestos>())
+            {
+                try
+                {
+                    Result = repoitorio.ListaGet().ToList();
+                }
+                catch { }
+                return Result;
+            }
+        }
+    }
+}
---
+++ DAL/Parcial1-Ap2-JoseGonzalezDb.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Entity;
+using Entidades;
+
+namespace DAL
+{
+    class Parcial1_Ap2_JoseGonzalezDb : DbContext
+    {
+        public Parcial1_Ap2_JoseGonzalezDb() : base("ConStr")
+        {
+
+        }
+
+        public virtual DbSet<Presupuestos> Presupuestos { get; set; }
+
+    }
+}
